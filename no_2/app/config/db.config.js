017c956194afe409eea2afc8a65f6e061cef2517
Donny Ostrianto Sundawa <donny.ostrianto@gmail.com>
2020-11-09 20:09:04 +0700

    update db to heroku config

--- no_2/app/config/db.config.js
+++ no_2/app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "donny",
-  PASSWORD: "P@ssw0rd",
-  DB: "cobain",
+  HOST: "us-cdbr-east-02.cleardb.com",
+  USER: "b741bdc37bb46a",
+  PASSWORD: "6dc171c7",
+  DB: "heroku_3a98471c9523e44",
 };
